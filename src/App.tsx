c1537d5ec5cb7b3d4f05c46ef11a1b3f5e8efffa
Audrey4545 <57604074+Audrey4545@users.noreply.github.com>
2025-12-18 18:40:31 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import GameSelect from "./pages/GameSelect";
+import ReflexTimer from "./pages/games/ReflexTimer";
+import ColorChange from "./pages/games/ColorChange";
+import KeywordGame from "./pages/games/KeywordGame";
+import DontClick from "./pages/games/DontClick";
+import SequenceFlash from "./pages/games/SequenceFlash";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,7 +22,12 @@ const App = () => (
       <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/games" element={<GameSelect />} />
+          <Route path="/game/reflex" element={<ReflexTimer />} />
+          <Route path="/game/color" element={<ColorChange />} />
+          <Route path="/game/keyword" element={<KeywordGame />} />
+          <Route path="/game/dontclick" element={<DontClick />} />
+          <Route path="/game/sequence" element={<SequenceFlash />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
